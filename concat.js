2b4a2dd06834cb73d273ec99aee44cc83829c0bc
Nicole Julek Klazura <143097595+nicjulek@users.noreply.github.com>
2023-10-11 17:58:59 -0300

    exercicios e exemplos

---
+++ concat.js
@@ -0,0 +1,12 @@
+const salaJS = ["Evaldo", "Camis", "Mari"];
+const salaPython = ["Ju", "Leo", "Raquel"];
+
+const salasUnificadas = salaPython.concat(salaJS);
+
+console.log(salasUnificadas);
+
+const arrayOriginal = [50, 60, 70]
+const arrayConcat = arrayOriginal.concat([80, [90, 100]])
+
+console.log(arrayConcat)
+console.log(arrayOriginal)
